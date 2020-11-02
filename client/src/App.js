a432587dca086ffd7a59396c85f81ee6d46fa4ea
Mike Evlantev <mikeev21@gmail.com>
2020-11-02 14:38:30 -0800

    created and implemented footer and header with nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <Fragment>
-      <h1>Stor</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Stor</h1>
+        </Container>
+      </main>
+      <Footer />
     </Fragment>
   );
 }
