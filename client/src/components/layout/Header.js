f40a79451f2e243445c121c7ddef6898c961f19c
Mike Evlantev <mikeev21@gmail.com>
2020-11-03 09:44:14 -0800

    added new favicon

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <header>
       <Navbar bg="light" variant='light' expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">Stor</Navbar.Brand>
+          <Navbar.Brand href="/"><i className="fas fa-burn"></i>&nbsp;&nbsp;Stor</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
