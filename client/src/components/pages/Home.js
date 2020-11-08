b81d73cc5dbd7b067261c6d7292b1e33835b2bf7
Mike Evlantev <mikeev21@gmail.com>
2020-11-07 23:40:24 -0800

    added and implemented Product component

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,18 +1,21 @@
 import React, {Fragment} from 'react';
 import {Row, Col} from 'react-bootstrap';
+import Product from '../Product';
 import products from '../../products';
 
 const Home = () => {
   return (
     <Fragment>
       <h1>Latest Products</h1>
-      {products ? products.map(p =>
-        <Row>
-          <Col sm={12} md={6} lg={4} xl={3}>
-            {p.name}
-          </Col>
-        </Row>
-      ) : <span>No products found</span>}  
+      <Row>
+        {products ? products.map(p =>
+          
+            <Col sm={12} md={6} lg={4} xl={3}>
+              <Product product={p} />
+            </Col>
+          
+        ) : <span>No products found</span>}
+      </Row>
     </Fragment>
   )
 }
