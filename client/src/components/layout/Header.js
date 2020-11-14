30a4d9066dde51a7702508e03de13b98a82b899c
Mike Evlantev <mikeev21@gmail.com>
2020-11-14 15:22:03 -0800

    replaced anchor tags with router Links

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import {Navbar, Nav, Container} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="light" variant='light' expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/"><i className="fas fa-burn"></i>&nbsp;&nbsp;Stor</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand><i className="fas fa-burn"></i>&nbsp;&nbsp;Stor</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart">
-                <i className="fas fa-shopping-basket"></i>
-                &nbsp;&nbsp;Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <i className="fas fa-user"></i>
-                &nbsp;&nbsp;Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-basket"></i>
+                  &nbsp;&nbsp;Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i>
+                  &nbsp;&nbsp;Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
