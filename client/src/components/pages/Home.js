161143a73b6b8d7f7cb0ae1d29198b1c64bb9281
Mike Evlantev <mikeev21@gmail.com>
2020-11-23 21:47:26 -0800

    consuming redux state: listing products on the home page; added spinner

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,33 +1,36 @@
-import React, {Fragment, useState, useEffect} from 'react';
-import {Row, Col} from 'react-bootstrap';
+import React, {Fragment, useEffect} from 'react';
+import {Row, Col, Spinner} from 'react-bootstrap';
+import {useDispatch, useSelector} from 'react-redux';
 import Product from '../Product';
-import axios from 'axios';
+import {listProducts} from '../../actions/productActions.js';
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
-
+  const dispatch = useDispatch();
+  const productList = useSelector(state => state.productList); // bring in the product list part of the state
+  const {loading, error, products} = productList;
   // runs as soon as component loads
   useEffect(() => {
-    const fetchProducts = async () => {
-      const {data} = await axios.get('/api/products');
-      setProducts(data);
-    }    
-    
-    fetchProducts();
-  }, []);
+    dispatch(listProducts()); // calls api
+  }, [dispatch]);
 
   return (
-    <Fragment>
-      <h1>Latest Products</h1>
-      <Row>
-        {products ? products.map((p, i) =>
-          
-            <Col key={i} sm={12} md={6} lg={4} xl={3}>
-              <Product product={p} />
-            </Col>
-          
-        ) : <span>No products found</span>}
-      </Row>
+    <Fragment>      
+      {loading 
+        ? (<Spinner animation='border' variant='primary' />)
+        : error 
+          ? (<h3>{error}</h3>)
+          : (
+            <Fragment>
+              <h1>Latest Products</h1>
+              <Row>
+                {products ? products.map((p, i) =>                  
+                  <Col key={i} sm={12} md={6} lg={4} xl={3}>
+                    <Product product={p} />
+                  </Col>                  
+                ) : <span>No products found</span>}
+              </Row>
+            </Fragment>
+          )} 
     </Fragment>
   )
 }
