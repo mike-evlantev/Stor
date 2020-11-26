4bc8409c3e21e415beb87b1cdbc8252473e801e9
Mike Evlantev <mikeev21@gmail.com>
2020-11-25 22:21:14 -0800

    initial Bag page

--- client/src/components/pages/Bag.js
+++ client/src/components/pages/Bag.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import {Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap';
+import {Message} from '../Message.js';
+import {addToBag} from '../../actions/bagActions.js'; 
 
-const Bag = () => {
+const Bag = ({match, location, history}) => {
+  const bag = useSelector(state => state.bag);
+  const {bagItems} = bag;
+
+  const dispatch = useDispatch();
+
   return (
-    <div>
-      Bag
-    </div>
+    <Row>
+      <Col md={8}>
+        <h1>My Bag</h1>
+      </Col>
+      <Col md={2}>
+      </Col>
+      <Col md={2}>
+        Items: {bagItems.length}
+      </Col>
+    </Row>
   )
 };
 
