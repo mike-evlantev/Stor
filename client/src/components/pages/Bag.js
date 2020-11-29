f819d48850f046654021baa63473f22700de243d
Mike Evlantev <mikeev21@gmail.com>
2020-11-29 13:28:26 -0800

    modified bag items layout; added initial order summary; added button actions; removed unused references

--- client/src/components/pages/Bag.js
+++ client/src/components/pages/Bag.js
@@ -1,12 +1,11 @@
-import React, {Fragment, useEffect} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
-import {Row, Col, ListGroup, Image, Form, Button, Card, Container} from 'react-bootstrap';
+import {Row, Col, ListGroup, Image, Button, Card, Container} from 'react-bootstrap';
 import QtySelect from '../QtySelect.js';
-import {Message} from '../Message.js';
-import {addToBag} from '../../actions/bagActions.js'; 
+import {addToBag, removeFromBag} from '../../actions/bagActions.js'; 
 
-const Bag = ({match, location, history}) => {
+const Bag = ({history}) => {
   const bag = useSelector(state => state.bag);
   const {bagItems} = bag;
 
@@ -20,14 +19,21 @@ const Bag = ({match, location, history}) => {
     dispatch(addToBag(productId, Number(e.target.value)));
   }
 
+  const handleRemoveItemFromBag = (productId) => {
+    dispatch(removeFromBag(productId));
+  };
+
+  const handleCheckout = () => {
+    history.push('/login?redirect=checkout');
+  };
+
   return(
-    <Container className='d-flex'>
-    {bagItems.length === 0 
-      ? <div className='mx-auto text-center py-5'>
-          <Row>
-            <h1>You haven't added anything to your bag yet!</h1>
-          </Row>
-          <span>
+    <Container className='d-flex py-5'>
+      {bagItems.length === 0 
+        ? <div className='mx-auto text-center'>
+            <Row className='py-5'>
+              <h1>You haven't added anything to your bag yet!</h1>
+            </Row>
             <Button 
               type='button' 
               variant='primary' 
@@ -36,39 +42,79 @@ const Bag = ({match, location, history}) => {
               <i className='fas fa-burn text-inverse'></i>
               &nbsp;&nbsp;
               SHOP NOW
-            </Button>
-          </span>          
-        </div>
-      : <Row>
-          <Col md={8}>
-            <h1>My Bag</h1>
-            <ListGroup variant='flush'>
-              {bagItems.map(item => (
-                <ListGroup.Item key={item.product}>
-                  <Row>
-                    <Col md={2}>
-                      <Image src={item.image} alt={item.name} fluid />
-                    </Col>
-                    <Col md={3}>
-                      <Link to={`/product/${item.product}`}>{item.name}</Link>
-                    </Col>
-                    <Col md={2}>
-                      ${item.price}
-                    </Col>
-                    <Col md={2}>
-                      <QtySelect product={item} qty={item.qty} onChange={handleQtyChange} />
-                    </Col>
-                  </Row>
+            </Button>        
+          </div>
+        : <Row className='py-3'>
+            <Col md={8}>
+              <h1>My Bag ({bagItems.length})</h1>
+              <hr />
+              <ListGroup variant='flush'>
+                {bagItems.map(item => (
+                  <ListGroup.Item key={item.product}>
+                    <Card className='border-0'>
+                      <Row>
+                        <Col md={4}>
+                          <Image src={item.image} alt={item.name} fluid />
+                        </Col>
+                        <Col md={8}>
+                          <Card.Body>
+                            <Card.Title><Link to={`/product/${item.product}`}>{item.name}</Link></Card.Title>
+                            <Card.Text>Item Description</Card.Text>
+                            <Container>
+                              <Row>
+                                <Col lg={4}>
+                                  <div>Item Price:</div> 
+                                  <div>${item.price}</div>
+                                </Col>
+                                <Col lg={4}>
+                                  Qty: <QtySelect product={item} qty={item.qty} onChange={handleQtyChange} />
+                                </Col>
+                                <Col lg={4} className='d-flex'>
+                                  <Button variant="link" size="sm" className='my-auto' onClick={() => handleRemoveItemFromBag(item.product)}>
+                                    Remove
+                                  </Button>
+                                </Col>
+                              </Row>
+                            </Container>
+                          </Card.Body>
+                        </Col>
+                      </Row>
+                    </Card>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Col>
+            <Col md={4}>
+              <h2 className='py-1'>Order Summary</h2>
+              <hr />
+              <ListGroup variant='flush'>
+                <ListGroup.Item className='d-flex'>
+                  <div>Subtotal</div>
+                  <div className='ml-auto'>${bagItems.reduce((acc, item) => acc + (item.qty * item.price), 0).toFixed(2)}</div>
                 </ListGroup.Item>
-              ))}
-            </ListGroup>
-          </Col>
-          <Col md={2}>
-          </Col>
-          <Col md={2}>
-            Items: {bagItems.length}
-          </Col>
-        </Row>}
+                <ListGroup.Item className='d-flex'>
+                  <div>Shipping</div> 
+                  <div className='ml-auto'>FREE</div>
+                </ListGroup.Item>
+                <ListGroup.Item className='d-flex'>
+                  <div>Tax</div>
+                  <div className='ml-auto'>Calculated at checkout</div>
+                </ListGroup.Item>
+                <ListGroup.Item className='d-flex'>
+                  <div><strong>Estimated Total</strong></div>
+                  <div className='ml-auto'><strong>$1.00</strong></div>
+                </ListGroup.Item>
+              </ListGroup>
+              <Button 
+                type='button' 
+                className='btn-block mt-4' 
+                disabled={bagItems.length === 0} 
+                onClick={handleCheckout}
+              >
+                <i className='fas fa-burn'></i>&nbsp;&nbsp;Checkout
+              </Button>
+            </Col>
+          </Row>}
     </Container>
   );
 };
