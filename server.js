c3a0a8992e39340f9d717404e4dbc5acccd6177f
Mike Evlantev <mikeev21@gmail.com>
2020-12-04 20:17:49 -0800

    added user router

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import colors from 'colors';
-import dotenv from 'dotenv';
-import connectDb from './config/db.js';
-import {notFound, errorHandler} from './middleware/errorHandler.js';
-//import authRouter from'./routes/auth.js';
-import productsRouter from'./routes/productRoutes.js';
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorHandler.js";
+import productsRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 dotenv.config();
 
 const app = express();
@@ -12,14 +12,21 @@ const app = express();
 // Database
 connectDb();
 
-// Routes
-//app.use('/api/auth', authRouter);
-app.use('/api/products', productsRouter);
-
+// Preserve order of app.use() statements below
 // Init middleware
 app.use(express.json({ extended: false })); // Allows to accept data within a body of a request (req.body)
+
+// Routes
+app.use("/api/products", productsRouter);
+app.use("/api/users", userRouter);
+
 app.use(notFound);
-app.use(errorHandler); 
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`${process.env.NODE_ENV.toUpperCase()} server started on port ${PORT}`.yellow.bold));
+app.listen(PORT, () =>
+  console.log(
+    `${process.env.NODE_ENV.toUpperCase()} server started on port ${PORT}`
+      .yellow.bold
+  )
+);
