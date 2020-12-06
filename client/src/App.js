6133b1cb9852bf09edb3e75a4790e86669a6d8d7
Mike Evlantev <mikeev21@gmail.com>
2020-12-06 11:42:38 -0800

    initial sign in page and ui route set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import {BrowserRouter as Router, Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import Header from './components/layout/Header.js';
-import Footer from './components/layout/Footer.js';
-import Home from './components/pages/Home.js';
-import Product from './components/pages/Product.js';
-import Bag from './components/pages/Bag.js';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/layout/Header.js";
+import Footer from "./components/layout/Footer.js";
+import Home from "./components/pages/Home.js";
+import Product from "./components/pages/Product.js";
+import Bag from "./components/pages/Bag.js";
+import SignIn from "./components/pages/SignIn.js";
 
 const App = () => {
   return (
@@ -13,14 +14,15 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path='/' component={Home} exact />
-          <Route path='/product/:id' component={Product} />
-          <Route path='/bag' component={Bag} />
+          <Route path="/" component={Home} exact />
+          <Route path="/product/:id" component={Product} />
+          <Route path="/bag" component={Bag} />
+          <Route path="/login" component={SignIn} />
         </Container>
       </main>
       <Footer />
     </Router>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/pages/SignIn.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignIn = () => {
+  return <div>Sign In Here</div>;
+};
+
+export default SignIn;
