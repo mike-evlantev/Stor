fa61d2f0c4a03052fa32321ff422e80d42074aa2
Mike Evlantev <mikeev21@gmail.com>
2020-12-05 20:34:11 -0800

    created route and persistence logic for user registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,30 @@ import asyncHandler from "express-async-handler"; // instead of writing try/catc
 import { generateToken } from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
+// @route       POST /api/users
+// @desc        Register user and get token
+// @access      Public
+export const registerUser = asyncHandler(async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  let user = await User.findOne({ email });
+  if (user) return res.status(400).json({ msg: "User already exists" });
+  user = await User.create({ firstName, lastName, email, password }); // Password is encrypted as part of the User model middleware
+  if (user) {
+    res.json({
+      _id: user._id,
+      firstName: user.firstName,
+      middleName: user.middleName,
+      lastName: user.lastName,
+      isActive: user.isActive,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid user data");
+  }
+});
+
 // @route       POST /api/users/login
 // @desc        Auth user and get token
 // @access      Public
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { loginUser, userProfile } from "../controllers/userController.js";
+import {
+  registerUser,
+  loginUser,
+  userProfile,
+} from "../controllers/userController.js";
 import { protect } from "../middleware/authHandler.js";
 const router = express.Router();
 
+router.post("/", registerUser);
 router.post("/login", loginUser);
 router.route("/profile").get(protect, userProfile);
 
