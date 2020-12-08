3eafc582a352d7bb1734703a6ffbb39b19eb8d34
Mike Evlantev <mikeev21@gmail.com>
2020-12-07 17:44:50 -0800

    prettier

--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -1,33 +1,37 @@
-import React from 'react'
+import React from "react";
 
-const Rating = ({value, numReviews}) => {
+const Rating = ({ value, numReviews }) => {
   const totalStars = 5;
   const ratingStars = [];
-  if (Number.isInteger(value)) { // true if 'value' is a whole number, false if 'value' is decimal
-    // add whole stars 
-    for (let i = 0; i < value; i++)
-    ratingStars.push('fas fa-star');
+  if (Number.isInteger(value)) {
+    // true if 'value' is a whole number, false if 'value' is decimal
+    // add whole stars
+    for (let i = 0; i < value; i++) ratingStars.push("fas fa-star");
   } else {
-    // add whole stars 
-    for (let i = 0; i < Math.floor(value); i++) // round down to the nearest whole number
-    ratingStars.push('fas fa-star');
+    // add whole stars
+    for (
+      let i = 0;
+      i < Math.floor(value);
+      i++ // round down to the nearest whole number
+    )
+      ratingStars.push("fas fa-star");
 
     // push half star
-    ratingStars.push('fas fa-star-half-alt');
+    ratingStars.push("fas fa-star-half-alt");
   }
 
   // the rest are empty stars
-  while(ratingStars.length < totalStars)
-  ratingStars.push('far fa-star');
-  
+  while (ratingStars.length < totalStars) ratingStars.push("far fa-star");
+
   return (
-    <span className='rating'>
-      {ratingStars.length > 0 && ratingStars.map((star, i) => (
-        <i key={i} className={`${star} text-warning`}></i>
-      ))}
+    <span className="rating">
+      {ratingStars.length > 0 &&
+        ratingStars.map((star, i) => (
+          <i key={i} className={`${star} text-warning`}></i>
+        ))}
       &nbsp;({numReviews})
     </span>
-  )
-}
+  );
+};
 
-export default Rating
+export default Rating;
