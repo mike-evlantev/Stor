1dcb08aa8f5de2f185a2dd47f24ffeecc53dc1e9
Mike Evlantev <mikeev21@gmail.com>
2020-12-11 20:30:32 -0800

    user registration step 3: register (and login) action

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,10 +1,51 @@
 import axios from "axios";
 import {
+  REGISTER_REQUEST,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
   LOGIN_REQUEST,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
 } from "../constants/userConstants.js";
+
+export const register = (email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: REGISTER_REQUEST });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      "/api/users",
+      { email, password },
+      config
+    );
+
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: data,
+    });
+
+    // also log in user on registration
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem("loggedInUser", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -38,7 +79,7 @@ export const login = (email, password) => async (dispatch) => {
   }
 };
 
-export const logout = () => async (dispatch) => {
+export const logout = () => (dispatch) => {
   localStorage.removeItem("loggedInUser");
   dispatch({ type: LOGOUT });
 };
