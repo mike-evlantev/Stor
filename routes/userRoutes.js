f7e000f07974a71f228be6d10793bb6b1a52b8b4
Mike Evlantev <mikeev21@gmail.com>
2020-12-12 21:22:23 -0800

    added update profile api route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,13 +2,15 @@ import express from "express";
 import {
   registerUser,
   loginUser,
-  userProfile,
+  getUserProfile,
+  updateUserProfile,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authHandler.js";
 const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
-router.route("/profile").get(protect, userProfile);
+router.route("/profile").get(protect, getUserProfile);
+router.route("/profile").put(protect, updateUserProfile);
 
 export default router;
