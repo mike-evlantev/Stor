4a6db376404c774344fb5b725cbeb1de9df71339
Mike Evlantev <mikeev21@gmail.com>
2020-12-12 21:17:52 -0800

    added profile route to react router + minor clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import Header from "./components/layout/Header.js";
-import Footer from "./components/layout/Footer.js";
-import Home from "./components/pages/Home.js";
-import Product from "./components/pages/Product.js";
+
 import Bag from "./components/pages/Bag.js";
-import SignIn from "./components/pages/SignIn.js";
+import Footer from "./components/layout/Footer.js";
+import Header from "./components/layout/Header.js";
+import Home from "./components/pages/Home.js";
 import PrivacyPolicy from "./components/pages/PrivacyPolicy.js";
+import Product from "./components/pages/Product.js";
+import Profile from "./components/pages/Profile.js";
+import SignIn from "./components/pages/SignIn.js";
 import TermsOfUse from "./components/pages/TermsOfUse.js";
 
 const App = () => {
@@ -17,10 +19,11 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Route path="/" component={Home} exact />
-          <Route path="/product/:id" component={Product} />
           <Route path="/bag" component={Bag} />
           <Route path="/login" component={SignIn} />
           <Route path="/privacy" component={PrivacyPolicy} />
+          <Route path="/product/:id" component={Product} />
+          <Route path="/profile" component={Profile} />
           <Route path="/terms" component={TermsOfUse} />
         </Container>
       </main>
