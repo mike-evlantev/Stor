2f56cc9bf223d4ebc5e68363ca6b5aa83b2a5271
Mike Evlantev <mikeev21@gmail.com>
2020-12-13 21:05:30 -0800

    get and update user profile step 3: add and implement actions

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -7,6 +7,12 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
+  UPDATE_PROFILE_REQUEST,
+  UPDATE_PROFILE_SUCCESS,
+  UPDATE_PROFILE_FAIL,
+  GET_PROFILE_REQUEST,
+  GET_PROFILE_SUCCESS,
+  GET_PROFILE_FAIL,
 } from "../constants/userConstants.js";
 
 export const register = (email, password) => async (dispatch) => {
@@ -82,4 +88,74 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
   localStorage.removeItem("loggedInUser");
   dispatch({ type: LOGOUT });
+};
+
+export const getProfile = (id) => async (dispatch, getState) => {
+  try {
+    const {
+      auth: { loggedInUser },
+    } = getState();
+    const isLoggedInUser = loggedInUser._id === id;
+    if (!isLoggedInUser) throw new Error("Failed to get user profile");
+
+    dispatch({ type: GET_PROFILE_REQUEST });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${loggedInUser.token}`,
+      },
+    };
+
+    const { data } = await axios.get("/api/users/profile", config);
+
+    dispatch({
+      type: GET_PROFILE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_PROFILE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const updateProfile = (userProfile) => async (dispatch, getState) => {
+  try {
+    const {
+      auth: { loggedInUser },
+    } = getState();
+    const isLoggedInUser = loggedInUser._id === userProfile._id;
+    console.log(isLoggedInUser);
+
+    console.log("Update Profile called...");
+
+    if (!isLoggedInUser) throw new Error("Failed to update user profile");
+
+    dispatch({ type: UPDATE_PROFILE_REQUEST });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${loggedInUser.token}`,
+      },
+    };
+
+    const { data } = await axios.put("/api/users/profile", userProfile, config);
+
+    dispatch({
+      type: UPDATE_PROFILE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_PROFILE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
