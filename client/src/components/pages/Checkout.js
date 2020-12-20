1f399b26db252b8e0de4d36a0b1199924ddaca68
Mike Evlantev <mikeev21@gmail.com>
2020-12-20 14:33:17 -0800

    order summary layout and bag item details toggle

--- client/src/components/pages/Checkout.js
+++ client/src/components/pages/Checkout.js
@@ -1,16 +1,9 @@
-import React from "react";
-import {
-  Accordion,
-  Card,
-  Col,
-  Container,
-  Image,
-  ListGroup,
-  Row,
-} from "react-bootstrap";
+import React, { useState } from "react";
+import { Card, Col, Container, Image, ListGroup, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 const Checkout = () => {
+  const [bagItemsVisible, setBagItemsVisible] = useState(true);
   const bag = useSelector((state) => state.bag);
   const { bagItems, subtotal, shipping, tax, total } = bag;
 
@@ -32,60 +25,50 @@ const Checkout = () => {
           </ListGroup>
         </Col>
         <Col md={5}>
-          <h2 className="py-1">Order Summary</h2>
-          <Accordion defaultActiveKey="0">
-            <Accordion.Toggle>
-              <i className="fas fa-shopping-bag fa-lg"></i>&nbsp;&nbsp;
-              {bagItems.length} {bagItems.length > 1 ? "items" : "item"}
-              <text className="float-right">${total.toFixed(2)}</text>
-            </Accordion.Toggle>
-            <Accordion.Collapse eventKey="0">
-              <ListGroup variant="flush">
-                {bagItems &&
-                  bagItems.length > 0 &&
-                  bagItems.map((item) => (
-                    <ListGroup.Item key={item.product}>
-                      <Row>
-                        <Col md={3}>
-                          <Image src={item.image} alt={item.name} fluid />
-                        </Col>
-                        <Col md={9}></Col>
-                      </Row>
-                      <Card className="border-0">
-                        <Row>
-                          <Col md={3}>
-                            <Image src={item.image} alt={item.name} fluid />
-                          </Col>
-                          <Col md={9}>
-                            <Card.Body>
-                              <Card.Title>
-                                <strong>{item.name}</strong>
-                              </Card.Title>
-                              <Card.Text className="text-muted">
-                                SKU# {generateSkuCode(1000000000, 9999999999)}
-                              </Card.Text>
-                              <Container>
-                                <Row>
-                                  <Col lg={4}>
-                                    <div>Item Price:</div>
-                                    <div>${item.price}</div>
-                                  </Col>
-                                  <Col lg={4}>Qty:{item.qty}</Col>
-                                  <Col lg={4} className="d-flex">
-                                    Was a button
-                                  </Col>
-                                </Row>
-                              </Container>
-                            </Card.Body>
-                          </Col>
-                        </Row>
-                      </Card>
-                    </ListGroup.Item>
-                  ))}
-              </ListGroup>
-            </Accordion.Collapse>
-          </Accordion>
+          <h2 className="py-1 px-3">Order Summary</h2>
+          {/* key is necessary on parent element to be able to re-render child element: 
+              in this case, the chevron (up/down) icon next to item qty
+              https://reactjs.org/docs/reconciliation.html */}
+          <div
+            className="py-2 px-3"
+            key={bagItemsVisible}
+            onClick={() => setBagItemsVisible(!bagItemsVisible)}
+          >
+            <i className="fas fa-shopping-bag fa-lg"></i>&nbsp;&nbsp;
+            {bagItems.length} {bagItems.length > 1 ? "items" : "item"}
+            &nbsp;&nbsp;
+            <i
+              className={
+                bagItemsVisible
+                  ? "fas fa-chevron-up fa-lg"
+                  : "fas fa-chevron-down fa-lg"
+              }
+            ></i>
+            <strong className="float-right pr-1">${total.toFixed(2)}</strong>
+          </div>
           <ListGroup variant="flush">
+            {bagItemsVisible &&
+              bagItems &&
+              bagItems.length > 0 &&
+              bagItems.map((item) => (
+                <ListGroup.Item key={item.product}>
+                  <Row>
+                    <Col md={3}>
+                      <Image src={item.image} alt={item.name} fluid />
+                    </Col>
+                    <Col md={9} className="d-flex flex-column p-0">
+                      <strong>{item.name}</strong>
+                      <span className="text-muted">
+                        SKU# {generateSkuCode(1000000000, 9999999999)}
+                      </span>
+                      <div>
+                        <span>Qty:{item.qty}</span>
+                        <span className="float-right pr-3">${item.price}</span>
+                      </div>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
             <ListGroup.Item className="d-flex">
               <div>Subtotal</div>
               <div className="ml-auto">${subtotal.toFixed(2)}</div>
