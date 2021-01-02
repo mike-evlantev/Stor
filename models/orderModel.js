f3bc5e256d71169db01b461403f86a48fb842e87
Mike Evlantev <mikeev21@gmail.com>
2021-01-01 21:33:53 -0800

    added f,m,l name for non-user orders; added shipping schema to reflect price, timeframe, and shipping method

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,80 +1,108 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const orderItemSchema = mongoose.Schema({
-  name: {type: String, required: true},
-  qty: {type: Number, required: true},
-  image: {type: String, required: true},
-  total: {type: String, required: true},
-  product: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'Product' // reference specific model
+const orderItemSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    qty: { type: Number, required: true },
+    image: { type: String, required: true },
+    total: { type: String, required: true },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Product", // reference specific model
+    },
+  },
+  {
+    timestamps: true,
   }
-},{
-  timestamps: true
-});
+);
 
-const addressSchema = mongoose.Schema({
-  address1: {type: String, required: true},
-  address2: {type: String, required: false},
-  city: {type: String, required: true},
-  state: {type: String, required: true},
-  zip: {type: String, required: true}
-},{
-  timestamps: true
-});
+const addressSchema = mongoose.Schema(
+  {
+    address1: { type: String, required: true },
+    address2: { type: String, required: false },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zip: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const orderSchema = mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User' // reference specific model
+const shippingSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    timeframe: { type: String, required: true },
+    price: { type: Number, required: true },
   },
-  orderItems: [orderItemSchema],
-  shippingAddress: addressSchema,
-  paymentMethod: {
-    type: String,
-    required: true
+  {
+    timestamps: true,
+  }
+);
+
+const orderSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    middleName: {
+      type: String,
+      required: false,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User", // reference specific model
+    },
+    orderItems: [orderItemSchema],
+    shippingAddress: addressSchema,
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    payPalPaymentResult: {
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
+    },
+    taxAmount: {
+      type: Number,
+      default: 0.0,
+      required: true,
+    },
+    shipping: shippingSchema,
+    totalAmount: {
+      type: Number,
+      default: 0.0,
+      required: true,
+    },
+    isPaid: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    paymentDate: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    deliveryDate: {
+      type: Date,
+    },
   },
-  payPalPaymentResult: {
-    id: {type: String},
-    status: {type: String},
-    update_time: {type: String},
-    email_address: {type: String},
-  },
-  taxPrice: {
-    type: Number,
-    default: 0.0,
-    required: true,
-  },
-  shippingPrice: {
-    type: Number,
-    default: 0.0,
-    required: true,
-  },
-  totalPrice: {
-    type: Number,
-    default: 0.0,
-    required: true,
-  },
-  isPaid: {
-    type: Boolean,
-    default: false,
-    required: true,
-  },
-  paymentDate: {
-    type: Date,
-  },
-  isDelivered: {
-    type: Boolean,
-    default: false,
-    required: true,
-  },
-  deliveryDate: {
-    type: Date,
-  },
-}, {
-  timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("Order", orderSchema);
