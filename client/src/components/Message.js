0f752c577acc7f42303bba3a9339932b46110a11
Mike Evlantev <mikeev21@gmail.com>
2021-01-02 20:24:42 -0800

    updated alert to be visible for a limited time

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Alert } from "react-bootstrap";
 
-const Message = ({ variant, children }) => {
-  return <Alert variant={variant}>{children}</Alert>;
+const Message = ({ variant, delay, children }) => {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setVisible(false);
+    }, delay);
+  }, [delay]);
+  return visible ? <Alert variant={variant}>{children}</Alert> : null;
 };
 
 Message.defaultProps = {
   variant: "info",
+  delay: 5000,
 };
 
 export default Message;
