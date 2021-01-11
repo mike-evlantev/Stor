20f41d2c69b7e854c9ac9d5d46490115e9814e19
Mike Evlantev <mikeev21@gmail.com>
2021-01-10 21:10:17 -0800

    prettifier

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {Card} from 'react-bootstrap';
-import Rating from './Rating.js';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
+import Rating from "./Rating.js";
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
+  console.log(product);
   return (
-    <Card className='my-3 p-3 rounded'>
+    <Card className="my-3 p-3 rounded">
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
+        <Card.Img src={product.image} variant="top" />
       </Link>
 
       <Card.Body>
-        <Link to={`/product/${product._id}`} className='text-dark'>
-          <Card.Title as='div'>
+        <Link to={`/product/${product._id}`} className="text-dark">
+          <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
-        <Card.Text as='div'>
-          <Rating value={product.avgRating} numReviews={product.numReviews} /> 
+        <Card.Text as="div">
+          <Rating value={product.avgRating} numReviews={product.numReviews} />
         </Card.Text>
-        <Card.Text as='h5'>
-          ${product.price}  
-        </Card.Text>
+        <Card.Text as="h5">${product.price}</Card.Text>
       </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
