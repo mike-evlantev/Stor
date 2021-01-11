1d424892100f572ba32757db710df7d926aaaee7
Mike Evlantev <mikeev21@gmail.com>
2021-01-10 21:09:58 -0800

    significant user state management simplification

--- controllers/userController.js
+++ controllers/userController.js
@@ -62,82 +62,60 @@ export const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @route       GET /api/users/profile
-// @desc        Get user profile
+// @route       PUT /api/users/update
+// @desc        Update user
 // @access      Private
-export const getUserProfile = asyncHandler(async (req, res) => {
+export const updateUser = asyncHandler(async (req, res) => {
   const user = await User.findOne(req.user._id);
   if (user) {
-    res.json({
-      _id: user._id,
-      firstName: user.firstName,
-      middleName: user.middleName,
-      lastName: user.lastName,
-      address1: user.address1,
-      address2: user.address2,
-      city: user.city,
-      state: user.state,
-      zip: user.zip,
-      phone: user.phone,
-      email: user.email,
-      isActive: user.isActive,
-      isAdmin: user.isAdmin,
-    });
-  } else {
-    res.status(404);
-    throw new Error("User not found");
-  }
-});
+    if (req.body._id == req.user._id) {
+      const {
+        firstName,
+        middleName,
+        lastName,
+        address1,
+        address2,
+        city,
+        state,
+        zip,
+        phone,
+        // email, <-- cannot be updated. for now...
+        password,
+      } = req.body;
 
-// @route       PUT /api/users/profile
-// @desc        Update user profile
-// @access      Private
-export const updateUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findOne(req.user._id);
-  if (user && req.body._id === req.user._id) {
-    const {
-      firstName,
-      middleName,
-      lastName,
-      address1,
-      address2,
-      city,
-      state,
-      zip,
-      phone,
-      // email, <-- cannot be updated. for now...
-      password,
-    } = req.body;
+      // TODO: validate fields
 
-    // TODO: validate fields
+      user.firstName = firstName || user.firstName;
+      user.middleName = middleName || user.middleName;
+      user.lastName = lastName || user.lastName;
+      user.address1 = address1 || user.address1;
+      user.address2 = address2 || user.address2;
+      user.city = city || user.city;
+      user.state = state || user.state;
+      user.zip = zip || user.zip;
+      user.phone = phone || user.phone;
+      // if (password) user.password = password || user.password;
 
-    user.firstName = firstName || user.firstName;
-    user.middleName = middleName || user.middleName;
-    user.lastName = lastName || user.lastName;
-    user.address1 = address1 || user.address1;
-    user.address2 = address2 || user.address2;
-    user.city = city || user.city;
-    user.state = state || user.state;
-    user.zip = zip || user.zip;
-    user.phone = phone || user.phone;
-    // if (password) user.password = password || user.password;
-
-    const updatedUser = await user.save();
-    res.json({
-      _id: updatedUser._id,
-      firstName: updatedUser.firstName,
-      middleName: updatedUser.middleName,
-      lastName: updatedUser.lastName,
-      address1: updatedUser.address1,
-      address2: updatedUser.address2,
-      city: updatedUser.city,
-      state: updatedUser.state,
-      zip: updatedUser.zip,
-      phone: updatedUser.phone,
-      email: updatedUser.email,
-      isActive: updatedUser.isActive,
-      isAdmin: updatedUser.isAdmin,
-    });
+      const updatedUser = await user.save();
+      res.json({
+        _id: updatedUser._id,
+        firstName: updatedUser.firstName,
+        middleName: updatedUser.middleName,
+        lastName: updatedUser.lastName,
+        address1: updatedUser.address1,
+        address2: updatedUser.address2,
+        city: updatedUser.city,
+        state: updatedUser.state,
+        zip: updatedUser.zip,
+        phone: updatedUser.phone,
+        email: updatedUser.email,
+        isActive: updatedUser.isActive,
+        isAdmin: updatedUser.isAdmin,
+      });
+    } else {
+      res.status(403);
+      throw new Error("Unable to modify user");
+    }
   } else {
     res.status(404);
     throw new Error("User not found");
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,15 +2,13 @@ import express from "express";
 import {
   registerUser,
   loginUser,
-  getUserProfile,
-  updateUserProfile,
+  updateUser,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authHandler.js";
 const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
-router.route("/profile").get(protect, getUserProfile);
-router.route("/profile").put(protect, updateUserProfile);
+router.put("/update", protect, updateUser);
 
 export default router;
