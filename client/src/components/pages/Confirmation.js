0c12386eabebd99bae9c6127080968a6e10c846f
Mike Evlantev <mikeev21@gmail.com>
2021-01-16 21:16:05 -0800

    consume redux state for the order object

--- client/src/components/pages/Confirmation.js
+++ client/src/components/pages/Confirmation.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Fragment } from "react";
+import { Container } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import Loader from "../Loader";
 
 const Confirmation = () => {
-  return <div>Confirmation</div>;
+  const { order, success, loading } = useSelector((state) => state.submitOrder);
+  return (
+    <Container>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <div>Confirmation</div>
+          {success ? order._id : <div>Order not found</div>}
+        </Fragment>
+      )}
+    </Container>
+  );
 };
 
 export default Confirmation;
