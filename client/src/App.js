88462eee2711f66010ee14d09023301f1f390ca7
Mike Evlantev <mikeev21@gmail.com>
2021-03-07 13:34:07 -0800

    stripe elements setup on App load

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer.js";
 import Header from "./components/layout/Header.js";
 import { Routes } from "./components/routing/Routes.js";
 
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
 const App = () => {
+  // Avoid getting a new instance of 'stripePromise' all the time on render
+  // eslint-disable-next-line
+  const [stripePromise, setStripePromise] = useState(() =>
+    loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY)
+  );
+
   return (
     <Router>
       <Header />
-      <main className="py-3">
-        <Route component={Routes} />
-      </main>
+      <Elements stripe={stripePromise}>
+        <main className="py-3">
+          <Route component={Routes} />
+        </main>
+      </Elements>
       <Footer />
     </Router>
   );
