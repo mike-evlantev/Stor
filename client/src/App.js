9cace11f39e8eb4643bc20d2a6169d16a0406521
Mike Evlantev <mikeev21@gmail.com>
2022-09-13 20:51:21 -0700

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,13 @@ import Footer from "./components/layout/Footer.js";
 import Header from "./components/layout/Header.js";
 import { Routes } from "./components/routing/Routes.js";
 
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
-
 const App = () => {
-  // Avoid getting a new instance of 'stripePromise' all the time on render
-  // eslint-disable-next-line
-  // const [stripePromise, setStripePromise] = useState(() =>
-
-  // );
-
   return (
     <Router>
       <Header />
-      <Elements stripe={stripePromise}>
         <main className="py-3">
           <Route component={Routes} />
         </main>
-      </Elements>
       <Footer />
     </Router>
   );
