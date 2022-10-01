fa03037fef7f7d3363d8ae39e9a1dc76d4adb7ff
Mike Evlantev <mikeev21@gmail.com>
2022-09-30 21:20:18 -0700

    useEffect debugger utility

---
+++ client/src/utils/useEffectDebugger.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const usePrevious = (value, initialValue) => {
+  const ref = React.useRef(initialValue);
+  React.useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
+
+export const useEffectDebugger = (effectHook, dependencies, dependencyNames = []) => {
+    const previousDeps = usePrevious(dependencies, []);
+  
+    const changedDeps = dependencies.reduce((accum, dependency, index) => {
+      if (dependency !== previousDeps[index]) {
+        const keyName = dependencyNames[index] || index;
+        return {
+          ...accum,
+          [keyName]: {
+            before: previousDeps[index],
+            after: dependency
+          }
+        };
+      }
+  
+      return accum;
+    }, {});
+  
+    if (Object.keys(changedDeps).length) {
+      console.log('[use-effect-debugger] ', changedDeps);
+    }
+  
+    React.useEffect(effectHook, dependencies);
+  };
