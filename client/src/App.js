535a62ce8a55cdedd6c29ad30d2b592364beccae
Mike Evlantev <mikeev21@gmail.com>
2022-10-12 21:33:10 -0700

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer.js";
 import Header from "./components/layout/Header.js";
