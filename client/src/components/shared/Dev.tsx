ccec8bba8dee444978963e49cca1b9ccf4d93756
Mike Evlantev <mikeev21@gmail.com>
2022-10-22 00:23:09 -0700

    allow props from parent component

--- client/src/components/shared/Dev.tsx
+++ client/src/components/shared/Dev.tsx
@@ -4,14 +4,13 @@ interface DevProps {
     children: React.ReactNode;
 }
 
-export const Dev: React.FC<DevProps> = ({children}) => {
+export const Dev: React.FC<DevProps & React.HTMLAttributes<HTMLDivElement>> = ({children, ...props}) => {
     if (process.env.NODE_ENV !== 'development') {
         return null;
     }
-    //style={{backgroundColor: "gray", opacity: "0.5"}}
     
     return (
-        <div className="dev d-flex flex-column p-2" >
+        <div className={`dev ${props.className}`}>
            <strong>DEV</strong>
            {children}
         </div>
