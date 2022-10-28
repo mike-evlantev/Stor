e4802481022f6603081d23367d9925f9e52ee506
Mike Evlantev <mikeev21@gmail.com>
2022-10-27 22:58:01 -0700

    cleanup

--- client/src/components/pages/Checkout/SubmitOrderButton.tsx
+++ client/src/components/pages/Checkout/SubmitOrderButton.tsx
@@ -9,50 +9,13 @@ interface SubmitOrderButtonProps {
 }
 
 export const SubmitOrderButton = ({orderSubmitLoading, bagItems, handleSubmitOrder}: SubmitOrderButtonProps) => {
-    //const ccValidationInit: CreditCardValidation = { numberValid: false, expiryValid: false, cvcValid: false };
-    //const [ccValidation, setCcValidation] = React.useState<CreditCardValidation>(ccValidationInit);
-    //const elements = useElements();
-    const [ccValidation] = useCcFormValidation();
-    const {numberValid, expiryValid, cvcValid} = ccValidation;
-    //const isValid = (numberValid: boolean, expiryValid: boolean, cvcValid: boolean): boolean => numberValid && expiryValid && cvcValid;
-
-    // React.useEffect(() => {
-    //   console.log("useEffect validating card elements");
-
-    //   let cardNumberElement = elements?.getElement("cardNumber");
-    //   let cardExpiryElement = elements?.getElement("cardExpiry");
-    //   let cardCvvElement = elements?.getElement("cardCvc");
-
-    //   if (cardNumberElement) {
-    //     cardNumberElement.on("change", (e) => {
-    //       setCcValidation(prev => ({...prev, numberValid: !e.error && e.complete}));
-    //     });
-    //   }
-
-    //   if (cardExpiryElement) {
-    //     cardExpiryElement.on("change", (e) => {
-    //         setCcValidation(prev => ({...prev, expiryValid: !e.error && e.complete}));
-    //     });
-    //   }
-
-    //   if (cardCvvElement) {
-    //     cardCvvElement.on("change", (e) => {
-    //         setCcValidation(prev => ({...prev, cvcValid: !e.error && e.complete}));
-    //     });
-    //   }
-
-
-    // }, [elements]);
-
-    // validate cc form via stripe elements API
-    
     return (
       <Button
         variant="primary"
         className="my-2 float-right"
         onClick={() => handleSubmitOrder()}
         disabled={
-          orderSubmitLoading || !bagItems || bagItems.length === 0 || (!numberValid && !expiryValid && !cvcValid)
+          orderSubmitLoading || !bagItems || bagItems.length === 0
         }
       >
         Submit Order
