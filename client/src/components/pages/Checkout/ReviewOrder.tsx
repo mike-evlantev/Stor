361b4a2c9841b6c517724ae0e6c6757839812dd2
Mike Evlantev <mikeev21@gmail.com>
2022-10-30 20:44:05 -0700

    decided to focus on cc payments only for now. migrated to redux toolkit. cleanup.

--- client/src/components/pages/Checkout/ReviewOrder.tsx
+++ client/src/components/pages/Checkout/ReviewOrder.tsx
@@ -1,75 +1,42 @@
 import * as React from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { submitOrder } from "../../../actions/orderActions";
-import { PaymentMethod } from "../../../enums/PaymentMethod";
+import { useAppDispatch } from "../../../hooks/useAppDispatch";
+import { useAppSelector } from "../../../hooks/useAppSelector";
 import { useStripeMethods } from "../../../hooks/useStripeMethods";
 import { IShippingOption } from "../../../types/IShippingOption";
+import { submitOrder } from "../../../features/order/orderSlice";
 import { GoBackButton } from "./GoBackButton";
 import { SubmitOrderButton } from "./SubmitOrderButton";
+import { IOrder } from "../../../types/IOrder";
+import { PaymentMethod } from "@stripe/stripe-js";
 
-interface Props {
-    paymentMethod: PaymentMethod;
-    onStepChange: (step: number) => void;
-}
-
-export const ReviewOrder: React.FC<Props> = ({paymentMethod, onStepChange}) => {
-    const dispatch = useDispatch();
+export const ReviewOrder: React.FC = () => {
+    const dispatch = useAppDispatch();
     const history = useHistory();
     const { processStripeCreditCardPayment } = useStripeMethods();
-    const shippingOptions = useSelector((state: any) => state.shippingOptions);
-    const { bagItems, shipping, subtotal, tax, total } = useSelector((state: any) => state.bag);
-    const { loading: orderSubmitLoading } = useSelector((state: any) => state.order);
-    const customer = useSelector((state: any) => state.customer);
+    const { bagItems, shipping, subtotal, tax, total } = useAppSelector(state => state.bag);
+    const { loading: orderSubmitLoading } = useAppSelector(state => state.order);
+    const { first, last, shippingAddress, card } = useAppSelector(state => state.customer);
     
-    const processPaymentAsync = async () => {
-        let result = {} as any; // TODO: needs to be a proper type     
+    const processOrder = async () => {    
+        const order: IOrder = {
+            first,
+            last,
+            orderItems: bagItems,
+            subtotal,
+            tax,
+            total,
+            shippingAddress,
+            shippingOption: shipping as IShippingOption,
+            paymentMethod: card.paymentMethod as PaymentMethod
+        };
 
-        switch (paymentMethod) {
-            case PaymentMethod.CreditCard:
-                result = await processStripeCreditCardPayment();
-                break;
-            case PaymentMethod.PayPal:
-                // Process Paypal Payment
-                console.log("Processing Paypal Payment");
-                result.id = "";
-                result.status = "";
-                result.update_time = "";
-                result.email_address = "";
-                break;
-            default:
-                break;
-        }
-
-        //setPaymentResult(result);
-        console.log("result", result);
+        dispatch(submitOrder(order));
+        history.push("/confirmation");
     };
 
-      const processOrder = () => {
-        const { id, icon, ...shippingOption } = shippingOptions.find(
-            (o: IShippingOption) => o.id === 1//shippingOptionId
-        );
-    
-        const order = {
-            first: customer.first,
-            last: customer.last,
-            orderItems: bagItems,
-            taxAmount: tax,
-            totalAmount: total,
-            isPaid: true,
-            shippingAddress: customer.shippingAddress,
-            shippingOption,
-        };
-    
-        console.log(order);
-        // submit order
-        dispatch(submitOrder(history, order));
-        // clean up
-        //cleanUpOnSubmit();
-      };
-
     const handleSubmitOrder = async () => {
-        const paymentResult = await processPaymentAsync();
+        const paymentResult = await processStripeCreditCardPayment();  
         console.log(paymentResult);
         processOrder();
     };
@@ -78,7 +45,7 @@ export const ReviewOrder: React.FC<Props> = ({paymentMethod, onStepChange}) => {
         <>
             Order Review
             <div className="d-flex align-items-start">
-                <GoBackButton prevStep={2} handleClick={onStepChange} />
+                <GoBackButton prevStep={2} />
                 <SubmitOrderButton orderSubmitLoading={orderSubmitLoading} bagItems={bagItems} handleSubmitOrder={handleSubmitOrder} />
             </div>
         </>
