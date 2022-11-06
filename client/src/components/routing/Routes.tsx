df5d08f53fd04ec152c3184c856ee3c7c312b72b
Mike Evlantev <mikeev21@gmail.com>
2022-11-05 22:50:15 -0700

    fixed references and cleanup

--- client/src/components/routing/Routes.tsx
+++ client/src/components/routing/Routes.tsx
@@ -2,14 +2,14 @@ import * as React from "react";
 import { Container } from "react-bootstrap";
 import { Route, Switch } from "react-router-dom";
 import { Message } from "../layout/Message";
-import Order from "../Order";
-import Bag from "../pages/Bag";
+import { Bag } from "../pages/Bag/Bag";
 import { BillingInfoForm } from "../pages/Checkout/BillingInfoForm";
 import { Checkout } from "../pages/Checkout/Checkout";
 import { ReviewOrder } from "../pages/Checkout/ReviewOrder";
 import { ShippingInfoForm } from "../pages/Checkout/ShippingInfoForm";
+import { Confirmation } from "../pages/Confirmation";
 import { Dashboard } from "../pages/Dashboard";
-import Home from "../pages/Home";
+import { Home } from "../pages/Home";
 import { PageNotFound } from "../pages/PageNotFound";
 import { PrivacyPolicy } from "../pages/PrivacyPolicy";
 import { ProductDetails } from "../pages/Products/ProductDetails";
@@ -28,28 +28,23 @@ export const Routes: React.FC = () => {
                 <Route exact path="/bag" component={Bag} />
                 <Route
                     exact
-                    path="/checkout"
-                    render={(props) => <Order {...props} orderStage={"checkout"} />}
-                />
-                <Route
-                    exact
                     path="/checkout1"
-                    render={(props) => <Checkout><ShippingInfoForm /></Checkout>}
+                    render={() => <Checkout><ShippingInfoForm /></Checkout>}
                 />
                 <Route
                     exact
                     path="/checkout2"
-                    render={(props) => <Checkout><BillingInfoForm /></Checkout>}
+                    render={() => <Checkout><BillingInfoForm /></Checkout>}
                 />
                 <Route
                     exact
                     path="/checkout3"
-                    render={(props) => <ReviewOrder />}
+                    component={ReviewOrder}
                 />
                 <Route
                     exact
                     path="/confirmation"
-                    render={(props) => <Order {...props} orderStage={"confirmation"} />}
+                    render={() => <Confirmation />}
                 />
                 <Route exact path="/login" component={SignIn} />
                 <Route exact path="/privacy" component={PrivacyPolicy} />
