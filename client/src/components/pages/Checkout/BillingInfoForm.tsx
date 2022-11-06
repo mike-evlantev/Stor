a73bf132b47727de99c61b952a20cf1081998ea4
Mike Evlantev <mikeev21@gmail.com>
2022-11-05 22:48:44 -0700

    setup for stripe iframe inputs styling

--- client/src/components/pages/Checkout/BillingInfoForm.tsx
+++ client/src/components/pages/Checkout/BillingInfoForm.tsx
@@ -43,6 +43,31 @@ export const BillingInfoForm: React.FC = () => {
 
     const {createPaymentMethod} = useStripeMethods();
 
+    const appearance = {
+        theme: 'none',
+    
+        rules: {
+          '.Tab': {
+            border: '10px solid green',
+            boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 6px rgba(18, 42, 66, 0.02)',
+          },
+    
+          '.Tab:hover': {
+            color: 'var(--colorText)',
+          },
+    
+          '.Tab--selected': {
+            borderColor: '#E0E6EB',
+            boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 6px rgba(18, 42, 66, 0.02), 0 0 0 2px var(--colorPrimary)',
+          },
+    
+          '.Input--invalid': {
+            borderColor: 'green',
+            //boxShadow: '0 1px 1px 0 rgba(0, 0, 0, 0.07), 0 0 0 2px var(--colorDanger)',
+          }
+        }
+      };
+
     React.useEffect(() => {
         dispatch(updateCustomer({card: {...customer.card, ...creditCardValidation}}));
     }, [creditCardValidation]);
