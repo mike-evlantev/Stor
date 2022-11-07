c5d94e6fa95403864fc30998703f72f10d57fc75
Mike Evlantev <mikeev21@gmail.com>
2022-11-06 22:01:00 -0800

    redirect on pop when the user click back button after submitting and processing the order, we SHOULD always have a an order object on confirmation, reused Address component

--- client/src/components/pages/Confirmation.tsx
+++ client/src/components/pages/Confirmation.tsx
@@ -2,14 +2,25 @@ import * as React from "react";
 import { ListGroup } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import { useAppSelector } from "../../hooks/useAppSelector";
+import { IAddress } from "../../types/IAddress";
+import { IName } from "../../types/IName";
+import { Address } from "../shared/Address";
 import { Loader } from "../shared/Loader";
 
 export const Confirmation: React.FC = () => {
     const history = useHistory();
     const { success, loading, error, ...order } = useAppSelector(state => state.order);
 
-    console.log({ success, loading, error, ...order });
-    if (!loading && (!success || !order)) {
+    // If the user clicks the back button after submitting the order redirect home
+    React.useEffect(() => {
+        return () => {
+            if (history.action === "POP") {
+                history.push("/");
+            }
+        };
+    }, [history]);
+
+    if (!loading && (!success)) {
         history.push("/");
         return null;
     }
@@ -29,12 +40,12 @@ export const Confirmation: React.FC = () => {
                         <ListGroup.Item>
                             <h4 className="py-3">Order Information</h4>
                             <strong>
-                                <div>Order number: {order?.orderNumber}</div>
+                                <div>Order number: {order.orderNumber}</div>
                                 <div>
                                     Order date:{" "}
-                                    {new Date(order?.createdAt as string).toLocaleDateString(undefined, options)}
+                                    {new Date(order.createdAt as string).toLocaleDateString(undefined, options)}
                                 </div>
-                                <div>{order?.shippingOption?.name} ({order?.shippingOption?.timeframe})</div>
+                                <div>{order.shippingOption?.name} ({order.shippingOption?.timeframe})</div>
                             </strong>
                         </ListGroup.Item>
                     </ListGroup>
@@ -42,9 +53,7 @@ export const Confirmation: React.FC = () => {
                         <ListGroup.Item>
                             <h4 className="py-3">Shipping address</h4>
                             <strong>
-                                <div>{order?.first}&nbsp;{order?.last}</div>
-                                <div>{order?.shippingAddress?.address1}{", "}{order?.shippingAddress?.address2}</div>
-                                <div>{order?.shippingAddress?.city}{", "}{order?.shippingAddress?.state} {order?.shippingAddress?.zip}</div>
+                                <Address name={order as IName} address={order.shippingAddress as IAddress} />
                             </strong>
                         </ListGroup.Item>
                     </ListGroup>
