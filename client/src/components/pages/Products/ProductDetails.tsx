e5eba4987ea96c35508d20b22f1d3064dda8f419
Mike Evlantev <mikeev21@gmail.com>
2022-11-09 07:58:46 -0800

    fixed for 2 decimal currency display

--- client/src/components/pages/Products/ProductDetails.tsx
+++ client/src/components/pages/Products/ProductDetails.tsx
@@ -59,7 +59,7 @@ export const ProductDetails: React.FC<Props> = ({ match }) => {
                             <ListGroup.Item>
                                 <Rating value={0} numReviews={0}/>
                             </ListGroup.Item>
-                            <ListGroup.Item>${product.price}</ListGroup.Item>
+                            <ListGroup.Item>${product.price.toFixed(2)}</ListGroup.Item>
                             <ListGroup.Item>{product.description}</ListGroup.Item>
                         </ListGroup>
                     </Col>
@@ -70,7 +70,7 @@ export const ProductDetails: React.FC<Props> = ({ match }) => {
                                 <Row>
                                     <Col>Price:</Col>
                                     <Col>
-                                    <strong>${product.price}</strong>
+                                        <strong>${product.price.toFixed(2)}</strong>
                                     </Col>
                                 </Row>
                                 </ListGroup.Item>
@@ -78,27 +78,27 @@ export const ProductDetails: React.FC<Props> = ({ match }) => {
                                 <Row>
                                     <Col>Availability:</Col>
                                     <Col>
-                                    <strong className={inStockDisplayColor}>
-                                        {inStockDisplayName}
-                                    </strong>
+                                        <strong className={inStockDisplayColor}>
+                                            {inStockDisplayName}
+                                        </strong>
                                     </Col>
                                 </Row>
                                 </ListGroup.Item>
                                 {inStock && (
-                                <ListGroup.Item>
-                                    <Form.Group as={Row}>
-                                    <Form.Label column lg="6">
-                                        Qty:
-                                    </Form.Label>
-                                    <Col lg="6">
-                                        <QtySelect
-                                            product={product}
-                                            qty={quantity}
-                                            onChange={handleQuantityChange}
-                                        />
-                                    </Col>
-                                    </Form.Group>
-                                </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <Form.Group as={Row}>
+                                            <Form.Label column lg="6">
+                                                Qty:
+                                            </Form.Label>
+                                            <Col lg="6">
+                                                <QtySelect
+                                                    product={product}
+                                                    qty={quantity}
+                                                    onChange={handleQuantityChange}
+                                                />
+                                            </Col>
+                                        </Form.Group>
+                                    </ListGroup.Item>
                                 )}
                                 <ListGroup.Item>
                                     <Button
@@ -145,9 +145,9 @@ const AddToBagModal: React.FC<ModalProps> = ({bagItem, handleModalClose}) => {
                             <Table className="table-borderless table-hover">
                                 <thead>
                                     <tr>
-                                    <th colSpan={2}>
-                                        <h4>{bagItem.name}</h4>
-                                    </th>
+                                        <th colSpan={2}>
+                                            <h4>{bagItem.name}</h4>
+                                        </th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -157,11 +157,11 @@ const AddToBagModal: React.FC<ModalProps> = ({bagItem, handleModalClose}) => {
                                     </tr>
                                     <tr>
                                         <td>Price:</td>
-                                        <td>${bagItem.price}</td>
+                                        <td>${bagItem.price.toFixed(2)}</td>
                                     </tr>
                                     <tr>
                                         <td>Subtotal:</td>
-                                        <td>${bagItem.quantity * bagItem.price}</td>
+                                        <td>${(bagItem.quantity * bagItem.price).toFixed(2)}</td>
                                     </tr>
                                 </tbody>
                             </Table>
