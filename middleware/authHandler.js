1325a725a94064aa74acfc2d3a5aadc542981d2f
Mike Evlantev <mikeev21@gmail.com>
2022-11-13 14:42:29 -0800

    get orders api endpoint security (for admin accounts only)

--- middleware/authHandler.js
+++ middleware/authHandler.js
@@ -31,6 +31,32 @@ export const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
+export const admin = asyncHandler(async (req, res, next) => {
+  let token;
+  if (req.headers["x-auth-token"]) {
+    token = req.headers["x-auth-token"];
+  }
+  // Ensure token exists
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized to access route");
+  }
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select("-password");
+    if (!req.user.isAdmin) {
+      throw new Error("Not authorized to access route");
+    }
+
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401);
+    throw new Error("Not authorized to access route");
+  }
+});
+
 // Role-based authorization
 export const authorize = (...roles) => {
   return (req, res, next) => {
