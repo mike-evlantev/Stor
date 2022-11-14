65932048d0231f9f9644e085a8141820755bf147
Mike Evlantev <mikeev21@gmail.com>
2022-11-13 22:15:13 -0800

    added case for axios error

--- client/src/utils/errorUtils.ts
+++ client/src/utils/errorUtils.ts
@@ -1,9 +1,14 @@
+import axios from "axios";
+
 export const narrowError = (error: unknown): string => {
     let message: string;
     if (typeof error === "string") {
         message = error;
     } else if (error instanceof Error) {
         message = error.message;
+    } else if (axios.isAxiosError(error)) {
+        console.log("AxiosError", error);
+        message = error.response?.data.message;
     } else {
         message = String(error);
     }
