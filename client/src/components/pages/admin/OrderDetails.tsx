b73939cbbe9994bd3bb8be815eb8a00a8abc89bb
Mike Evlantev <mikeev21@gmail.com>
2022-11-13 22:11:56 -0800

    refactored admin order details to show as table with hoverable pictures

--- client/src/components/pages/admin/OrderDetails.tsx
+++ client/src/components/pages/admin/OrderDetails.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
-import { Col, Image } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { Image, Table } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
 import { getOrders } from "../../../features/admin/adminSlice";
 import { useAppDispatch } from "../../../hooks/useAppDispatch";
 import { useAppSelector } from "../../../hooks/useAppSelector";
 import { IAddress } from "../../../types/IAddress";
-import { IName } from "../../../types/IName";
 import { IOrder } from "../../../types/IOrder";
 import { Address } from "../../shared/Address";
 import { Last4 } from "../../shared/Last4";
@@ -38,100 +37,85 @@ export const OrderDetails: React.FC = () => {
 
 const OrderDetailsInternal = ({order}: {order: IOrder}) => {
     const { orderNumber, first, last, shippingAddress, shippingOption, orderItems, paymentMethod, subtotal, tax, total } = order;
-    const labelWidth = 3;
-    const contentWidth = 9;
+    
     return(
         <div className="d-flex flex-column">
-            <h5 className="mb-3">Order # {orderNumber}</h5>
+            <h5 className="mb-3">Order # {orderNumber}</h5>            
             <div className="d-flex align-items-start">
-                <Col sm={labelWidth}>
-                    Name
-                </Col>
-                <Col sm={contentWidth}>
-                    <div className="d-flex align-items-center">
-                        <div>{first} {last}</div>
-                    </div>
-                </Col>
-            </div>
-            <div className="d-flex align-items-start">
-                <Col sm={labelWidth}>
-                    Shipping
-                </Col>
-                <Col sm={contentWidth}>
-                    <Address name={undefined} address={shippingAddress as IAddress} />
-                </Col>
-            </div>
-            <div className="d-flex align-items-start">
-                <Col sm={labelWidth}>
-                    Shipping Option
-                </Col>
-                <Col sm={contentWidth}>
-                    <div className="d-flex align-items-center">
-                        <i className={shippingOption?.icon}></i>
-                        <div className="mx-2">{shippingOption?.timeframe}</div>
-                    </div>
-                </Col>
-            </div>
-            <div className="d-flex align-items-start">
-                <Col sm={labelWidth}>Items</Col>
-                <Col sm={contentWidth}>
-                    {orderItems?.map(item =>
-                        <div className="d-flex" key={item.id}>
-                            <Col md={3}>
-                                <Image src={item.image} alt={item.name} fluid />
-                            </Col>
-                            <Col md={9} className="d-flex flex-column" style={{paddingLeft: "0.75rem"}} >
-                                <strong>{item.name}</strong>
-                                <div className="d-flex mt-auto">
-                                    <span>Qty: {item.quantity}</span>
-                                    <span className="ms-auto">${item.price}</span>
+                <Table className="table-sm">
+                    <thead>
+                        <tr>
+                            <th colSpan={2}>Shipping</th>
+                            <th colSpan={2}>Billing</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td colSpan={2}>
+                                <Address name={{first, last}} address={shippingAddress as IAddress} />
+                            </td>
+                            <td colSpan={2}>
+                                <>
+                                    {paymentMethod.billing_details?.name && <div>{paymentMethod.billing_details.name}</div>}
+                                    <div>{paymentMethod.billing_details.address?.line1}{", "}{paymentMethod.billing_details.address?.line2}</div>
+                                    <div>{paymentMethod.billing_details.address?.city}{", "}{paymentMethod.billing_details.address?.state}&nbsp;{paymentMethod.billing_details.address?.postal_code}</div>
+                                </>                                
+                            </td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                    <thead>
+                        <tr>
+                            <th>Item#</th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th className="text-center">Qty</th>
+                            <th className="text-center">Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orderItems?.length > 0 && orderItems.map(o => 
+                            <tr key={o.id} className="align-middle">
+                                <td>
+                                    <Link to={`products/${o.id}`}>
+                                        {o.id}
+                                    </Link>
+                                </td>
+                                <td>
+                                    <Image width={50} src={o.image} alt={o.name} fluid className="hover-zoom" />
+                                </td>
+                                <td>{o.name}</td>
+                                <td className="text-center">{o.quantity}</td>
+                                <td className="text-end">${o.price.toFixed(2)}</td>
+                            </tr>)}
+                        <tr className="table-dark align-middle">
+                            <th colSpan={4}>Subtotal</th>
+                            <td className="text-end">${subtotal?.toFixed(2)}</td>
+                        </tr>
+                        <tr className="table-dark align-middle">
+                            <th colSpan={2}>Shipping</th>
+                            <td colSpan={2}>
+                                <div className="d-flex align-items-center">
+                                    <i className={shippingOption?.icon}></i>
+                                    <div className="mx-2">{shippingOption?.timeframe}</div>
                                 </div>
-                            </Col>
-                        </div>)}                                    
-                </Col>
-            </div>
-            <div className="d-flex align-items-start">
-                <Col sm={labelWidth}>
-                    Payment
-                </Col>
-                <Col sm={contentWidth}>
-                    <Last4 brand={paymentMethod?.card?.brand} last4={paymentMethod?.card?.last4} />                               
-                </Col>
-            </div>
-            {/* <div className="d-flex align-items-start">
-                <Col sm={labelWidth}>
-                    Billing
-                </Col>
-                <Col sm={contentWidth}>
-                    <Address name={{} as IName} address={billingAddress as IAddress} />
-                </Col>
-            </div> */}
-            <div className="d-flex align-items-start">
-                <Col sm={labelWidth}>Totals</Col>
-                <Col sm={contentWidth}>
-                    <hr />
-                    <div className="d-flex">
-                        <div>Subtotal</div>
-                        <div className="ms-auto">${subtotal?.toFixed(2)}</div>
-                    </div>
-                    <div className="d-flex">
-                        <div>Shipping</div>
-                        <div className="ms-auto">{shippingOption?.cost === 0 ? "FREE" : `$${shippingOption?.cost.toFixed(2)}`}</div>
-                    </div>
-                    <div className="d-flex">
-                        <div>Tax</div>
-                        <div className="ms-auto">${tax?.toFixed(2)}</div>
-                    </div>
-                    <hr />
-                    <div className="d-flex">
-                        <div>
-                            <strong>Order Total</strong>
-                        </div>
-                        <div className="ms-auto">
-                            <strong>${total?.toFixed(2)}</strong>
-                        </div>
-                    </div>                           
-                </Col>
+                            </td>
+                            <td className="text-end">{shippingOption?.cost === 0 ? "FREE" : `$${shippingOption?.cost.toFixed(2)}`}</td>    
+                        </tr>
+                        <tr className="table-dark align-middle">
+                            <th colSpan={4}>Tax</th>
+                            <td className="text-end">${tax?.toFixed(2)}</td>    
+                        </tr>
+                        <tr className="table-dark align-middle">
+                            <th colSpan={2}>Total</th>
+                            <td colSpan={2}>
+                                <Last4 boldText={false} brand={paymentMethod?.card?.brand} last4={paymentMethod?.card?.last4} />
+                            </td>
+                            <td className="text-end">${total?.toFixed(2)}</td>
+                        </tr>
+                    </tbody>
+                </Table>
             </div>
         </div>
     );
