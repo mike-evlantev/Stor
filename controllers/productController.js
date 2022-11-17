4b78fa647534f6d90d2408aa58bd9e9b14a18dd4
Mike Evlantev <mikeev21@gmail.com>
2022-11-16 21:55:21 -0800

    create/update product backend changes

--- controllers/productController.js
+++ controllers/productController.js
@@ -21,3 +21,27 @@ export const getProductById = asyncHandler(async (req, res) => {
     throw new Error("Product not found");
   }
 });
+
+// @route       POST api/products/create
+// @desc        Create product
+// @access      Private (Admin)
+export const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product(req.body);
+  const created = await product.save();
+  res.status(201).json(created);
+});
+
+// @route       PUT api/products/update
+// @desc        Update product
+// @access      Private (Admin)
+export const updateProduct = asyncHandler(async (req, res) => {
+  const product = req.body;
+  const filter = { _id: product.id };
+  const updated = await Product.findOneAndUpdate(filter, product, { new: true });
+  if (updated) {
+    res.json(updated);
+  } else {
+    res.status(500);
+    throw new Error("Unable to update product");
+  }
+});
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import {
-  getProductById,
-  getProducts,
-} from "../controllers/productController.js";
+import { getProductById, getProducts, createProduct, updateProduct } from "../controllers/productController.js";
+import { admin } from "../middleware/authHandler.js";
+
 const router = express.Router();
 
 router.route("/").get(getProducts);
 router.route("/:id").get(getProductById);
+router.post("/create", admin, createProduct);
+router.put("/update", admin, updateProduct);
 
 export default router;
