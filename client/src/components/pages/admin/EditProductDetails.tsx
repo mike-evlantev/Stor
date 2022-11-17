87f215ed4acc77c891e824787e0d7785eb68c721
Mike Evlantev <mikeev21@gmail.com>
2022-11-16 21:54:23 -0800

    product edit form component with confirmation modal hooked up to redux for updating current product and submitting update request

--- client/src/components/pages/admin/EditProductDetails.tsx
+++ client/src/components/pages/admin/EditProductDetails.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
-import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
+import { Button, Form, Image, InputGroup, Modal } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import { updateCurrentProduct, updateProduct } from "../../../features/admin/adminSlice";
 import { getProductById } from "../../../features/products/productsSlice";
 import { useAppDispatch } from "../../../hooks/useAppDispatch";
 import { useAppSelector } from "../../../hooks/useAppSelector";
+import { IKeyValuePair } from "../../../types/IKeyValuePair";
 import { Loader } from "../../shared/Loader";
 
 interface Params {
@@ -13,14 +15,23 @@ interface Params {
 export const EditProductDetails: React.FC = () => {
     const { id } = useParams<Params>();
     const dispatch = useAppDispatch();
-    const { loading, product } = useAppSelector(state => state.products);
+    const { loading, product } = useAppSelector(state => state.admin);
 
-    const inStock = product.countInStock > 0;
-    const inStockDisplayName = inStock ? "In Stock" : "Out of Stock";
-    const inStockDisplayColor = inStock ? "text-success" : "text-primary";
+    const [showModal, setShowModal] = React.useState(false);
+    const handleShow = () => setShowModal(true);
+    const handleHide = () => setShowModal(false);
 
-    const handleAddToBag = () => {
-        console.log("Button clicked");
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        const obj = {[name]: value} as IKeyValuePair<string>;
+
+        dispatch(updateCurrentProduct(obj));
+    };
+
+    const handleSubmit = async () => {
+        handleHide();
+        dispatch(updateProduct(product));
     }
 
     React.useEffect(() => {
@@ -29,57 +40,76 @@ export const EditProductDetails: React.FC = () => {
 
     return (
         <>
-            { loading 
+            {loading 
                 ? <Loader />
-                : <Row>
-                    <Col md={6}>
-                        <Image src={product.image} alt={product.name} fluid />
-                    </Col>
-                    <Col md={3}>
-                        <ListGroup variant="flush">
-                            <ListGroup.Item>
-                                <h3>{product.name}</h3>
-                            </ListGroup.Item>
-                            <ListGroup.Item>${product.price?.toFixed(2)}</ListGroup.Item>
-                            <ListGroup.Item>{product.description}</ListGroup.Item>
-                        </ListGroup>
-                    </Col>
-                    <Col>
-                        <Card>
-                            <ListGroup>
-                                <ListGroup.Item>
-                                <Row>
-                                    <Col>Price:</Col>
-                                    <Col>
-                                        <strong>${product.price?.toFixed(2)}</strong>
-                                    </Col>
-                                </Row>
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                <Row>
-                                    <Col>Availability:</Col>
-                                    <Col>
-                                        <strong className={inStockDisplayColor}>
-                                            {inStockDisplayName}
-                                        </strong>
-                                    </Col>
-                                </Row>
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <Button
-                                        className="btn-block"
-                                        type="button"
-                                        disabled={!inStock}
-                                        onClick={handleAddToBag}
-                                    >
-                                        Add to bag
-                                    </Button>
-                                </ListGroup.Item>
-                            </ListGroup>
-                        </Card>
-                    </Col>
-                </Row>
+                : <div className="d-flex flex-column">
+                    <Image src={product.image} alt={product.name} width={250} fluid />
+                    <Form.Group className="my-3">
+                        <Form.Control type="file" multiple />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" name="name" value={product.name} onChange={handleChange} placeholder="Name" />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control as="textarea" rows={3} name="description" value={product.description} onChange={handleChange} placeholder="Description" />
+                    </Form.Group>
+                    <div className="d-flex">
+                        <Form.Group className="mb-3 w-50">
+                            <Form.Label>Category</Form.Label>
+                            <Form.Control type="text" name="category" value={product.category} onChange={handleChange} placeholder="Category" />
+                        </Form.Group>
+                        <div className="mx-1"></div>
+                        <Form.Group className="mb-3 w-50">
+                            <Form.Label>Brand</Form.Label>
+                            <Form.Control type="text" name="brand" value={product.brand} onChange={handleChange} placeholder="Brand" />
+                        </Form.Group>
+                    </div>
+                    <div className="d-flex">
+                        <div className="d-flex flex-column w-50">
+                            <Form.Label>Price</Form.Label>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text>$</InputGroup.Text>
+                                <Form.Control name="price" value={product.price} onChange={handleChange} placeholder="Price" />
+                            </InputGroup>
+                        </div>
+                        <div className="mx-1"></div>
+                        <Form.Group className="mb-3 w-50">
+                            <Form.Label>In Stock</Form.Label>
+                            <Form.Control type="text" name="countInStock" value={product.countInStock} onChange={handleChange} placeholder="Quantity in stock" />
+                        </Form.Group>
+                    </div>
+                    <div className="d-flex">
+                        <Button className="ms-auto" onClick={handleShow}>Save</Button>
+                    </div>
+                </div>
             }
+            {showModal && <ConfirmationModal onConfirm={handleSubmit} onHide={handleHide} />}
         </>
     );
 };
+
+interface ModalProps {
+    onHide: () => void;
+    onConfirm: () => void;
+}
+
+const ConfirmationModal: React.FC<ModalProps> = ({onConfirm, onHide}) => {
+    return (
+        <Modal show size="lg" centered>
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Confirm
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <span>You sure, bro?</span>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="primary" onClick={onConfirm} className="ml-2">Confirm</Button>
+                <Button variant="secondary" onClick={onHide}>Cancel</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
