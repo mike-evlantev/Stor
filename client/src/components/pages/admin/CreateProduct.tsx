1067bbdd82e33be09387e15f1a3e021cefd6a438
Mike Evlantev <mikeev21@gmail.com>
2022-11-17 23:11:35 -0800

    added user id field to create order request, reusing product details form component, fixed uncontrolled-to-controlled state issue

--- client/src/components/pages/admin/CreateProduct.tsx
+++ client/src/components/pages/admin/CreateProduct.tsx
@@ -6,12 +6,14 @@ import { useAppSelector } from "../../../hooks/useAppSelector";
 import { IKeyValuePair } from "../../../types/IKeyValuePair";
 import { IProduct } from "../../../types/IProduct";
 import { Loader } from "../../shared/Loader";
+import { loggedInUserFromStorage } from "../../../utils/authUtils";
+import { ProductDetailsForm } from "./ProductDetailsForm";
 
 export const CreateProduct: React.FC = () => {
     const dispatch = useAppDispatch();
     const { loading } = useAppSelector(state => state.admin);
 
-    const [product, setProduct] = React.useState({} as IProduct);
+    const [product, setProduct] = React.useState<IProduct>({image: "", id: "", name: "", description: "", brand: "", category: "", price: 0, countInStock: 0});
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault();
@@ -22,55 +24,14 @@ export const CreateProduct: React.FC = () => {
     };
 
     const handleSubmit = async () => {
-        dispatch(createProduct(product));
+        dispatch(createProduct({...product, userId: loggedInUserFromStorage()._id}));
     }
 
     return (
         <>
             {loading 
                 ? <Loader />
-                : <div className="d-flex flex-column">
-                    <Image src={product.image} alt={product.name} width={250} fluid />
-                    <Form.Group className="my-3">
-                        <Form.Control type="file" multiple />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" name="name" value={product.name} onChange={handleChange} placeholder="Name" />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Description</Form.Label>
-                        <Form.Control as="textarea" rows={3} name="description" value={product.description} onChange={handleChange} placeholder="Description" />
-                    </Form.Group>
-                    <div className="d-flex">
-                        <Form.Group className="mb-3 w-50">
-                            <Form.Label>Category</Form.Label>
-                            <Form.Control type="text" name="category" value={product.category} onChange={handleChange} placeholder="Category" />
-                        </Form.Group>
-                        <div className="mx-1"></div>
-                        <Form.Group className="mb-3 w-50">
-                            <Form.Label>Brand</Form.Label>
-                            <Form.Control type="text" name="brand" value={product.brand} onChange={handleChange} placeholder="Brand" />
-                        </Form.Group>
-                    </div>
-                    <div className="d-flex">
-                        <div className="d-flex flex-column w-50">
-                            <Form.Label>Price</Form.Label>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>$</InputGroup.Text>
-                                <Form.Control name="price" value={product.price} onChange={handleChange} placeholder="Price" />
-                            </InputGroup>
-                        </div>
-                        <div className="mx-1"></div>
-                        <Form.Group className="mb-3 w-50">
-                            <Form.Label>In Stock</Form.Label>
-                            <Form.Control type="text" name="countInStock" value={product.countInStock} onChange={handleChange} placeholder="Quantity in stock" />
-                        </Form.Group>
-                    </div>
-                    <div className="d-flex">
-                        <Button className="ms-auto" onClick={handleSubmit}>Save</Button>
-                    </div>
-                </div>
+                : <ProductDetailsForm product={product} onChange={handleChange} onSave={handleSubmit} />
             }
         </>
     );
