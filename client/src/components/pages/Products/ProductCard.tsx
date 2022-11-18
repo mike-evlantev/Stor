1a8982a0b7db38c7bb963108d52454ac56f96989
Mike Evlantev <mikeev21@gmail.com>
2022-11-17 23:14:43 -0800

    more responsive product cards when images are different sizes

--- client/src/components/pages/Products/ProductCard.tsx
+++ client/src/components/pages/Products/ProductCard.tsx
@@ -17,7 +17,7 @@ export const ProductCard: React.FC<Props> = ({product}) => {
     }
     return(
         <Card className="my-3" onClick={() => goToProductDetails()} style={{cursor: "pointer"}}>
-            <Card.Img variant="top" src={image} />
+            <Card.Img variant="top" src={image} alt={name} style={{maxHeight: "233px", objectFit: "cover"}} />
             <Card.Body>
                 <Card.Title as="div" style={{minHeight: "75px"}}><strong>{name}</strong></Card.Title>
                 <Card.Text>
