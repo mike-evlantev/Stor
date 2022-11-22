52de4d063d1291487ed110f6250da2a9dedd2fce
Mike Evlantev <mikeev21@gmail.com>
2022-11-22 14:45:48 -0800

    initial rework for admin users component

--- client/src/components/pages/admin/Users.tsx
+++ client/src/components/pages/admin/Users.tsx
@@ -1,5 +1,26 @@
 import * as React from "react";
+import { useHistory } from "react-router-dom";
+import { getUsers } from "../../../features/admin/adminSlice";
+import { useAppDispatch } from "../../../hooks/useAppDispatch";
+import { useAppSelector } from "../../../hooks/useAppSelector";
+import { Loader } from "../../shared/Loader";
 
 export const Users: React.FC = () => {
-  return <div>Users</div>;
+	const history = useHistory();
+	const dispatch = useAppDispatch();
+	const { loading, users } = useAppSelector(state => state.admin);
+
+	React.useEffect(() => {
+		dispatch(getUsers());
+	}, [dispatch]);
+
+	return <>
+		{loading
+                ? <Loader />
+                : <div>
+					{users?.map(u => <p>
+						{u.id}
+					</p>)}
+				</div>}
+	</>;
 };
