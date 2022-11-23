a499fa1c2f3c7997e03f6b29cde6704a06447b69
Mike Evlantev <mikeev21@gmail.com>
2022-11-22 22:19:24 -0800

    separated out state abbreviations

--- client/src/components/shared/StateSelect.tsx
+++ client/src/components/shared/StateSelect.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Form } from "react-bootstrap";
+import { stateAbbreviations } from "../../constants/states";
 
 interface StateSelectProps {
     selectedState: string;
@@ -9,72 +10,7 @@ interface StateSelectProps {
 }
 
 const StateSelect: React.FC<StateSelectProps> = ({ selectedState, onBlur, onChange, error }) => {
-  // 50 states +
-  // American Samoa (AS)      District of Columbia (DC)     Federated States Of Micronesia (FM)
-  // Guam (GU)                Marshall Islands (MH)         Northern Mariana Islands (MP)
-  // Palau (PW)               Puerto Rico (PR)              Virgin Islands (VI)
-  const stateAbbreviations = [
-    "AL",
-    "AK",
-    "AS",
-    "AZ",
-    "AR",
-    "CA",
-    "CO",
-    "CT",
-    "DE",
-    "DC",
-    "FM",
-    "FL",
-    "GA",
-    "GU",
-    "HI",
-    "ID",
-    "IL",
-    "IN",
-    "IA",
-    "KS",
-    "KY",
-    "LA",
-    "ME",
-    "MH",
-    "MD",
-    "MA",
-    "MI",
-    "MN",
-    "MS",
-    "MO",
-    "MT",
-    "NE",
-    "NV",
-    "NH",
-    "NJ",
-    "NM",
-    "NY",
-    "NC",
-    "ND",
-    "MP",
-    "OH",
-    "OK",
-    "OR",
-    "PW",
-    "PA",
-    "PR",
-    "RI",
-    "SC",
-    "SD",
-    "TN",
-    "TX",
-    "UT",
-    "VT",
-    "VI",
-    "VA",
-    "WA",
-    "WV",
-    "WI",
-    "WY",
-  ];
-
+  
   const defaultSelectValue = "Select..."; // used in services/formValidator
 
   return (
