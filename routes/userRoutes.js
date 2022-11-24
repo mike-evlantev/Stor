28fe35f2d9923ffeb7e019ba0f73a9604b765e52
Mike Evlantev <mikeev21@gmail.com>
2022-11-23 21:39:24 -0800

    fix for api endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { registerUser, loginUser, updateUser, getUsers, getUserById } from "../controllers/userController.js";
+import { registerUser, loginUser, updateUser, getUsers, getUserById, createUser } from "../controllers/userController.js";
 import { protect, admin } from "../middleware/authHandler.js";
 const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
+router.post("/create", admin, createUser);
 router.put("/update", protect, updateUser);
 router.get("/", admin, getUsers);
 router.get("/:id", admin, getUserById);
