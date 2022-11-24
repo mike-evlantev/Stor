962aecd7e6b2addb3e54f3e075a6b67c47714528
Mike Evlantev <mikeev21@gmail.com>
2022-11-23 21:39:43 -0800

    fix for expected token header on protected routes

--- middleware/authHandler.js
+++ middleware/authHandler.js
@@ -4,15 +4,8 @@ import User from "../models/userModel.js";
 
 export const protect = asyncHandler(async (req, res, next) => {
   let token;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    // Sets auth header value 'Bearer <token>' to array ['Bearer', <token>]
-    // and grabs the <token> ([1])
-    token = req.headers.authorization.split(" ")[1];
-    // } else if (req.cookies.token) {
-    //   token = req.cookies.token;
+  if (req.headers["x-auth-token"]) {
+    token = req.headers["x-auth-token"];
   }
   // Ensure token exists
   if (!token) {
