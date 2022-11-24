dcbc603202a532767274d71410953226a4041249
Mike Evlantev <mikeev21@gmail.com>
2022-11-23 23:26:31 -0800

    separated out user form

--- client/src/components/pages/admin/EditUser.tsx
+++ client/src/components/pages/admin/EditUser.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
-import { Button, Col, Form, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import { stateAbbreviations } from "../../../constants/states";
 import { getUserById, updateCurrentUser, updateUser } from "../../../features/admin/adminSlice";
 import { useAppDispatch } from "../../../hooks/useAppDispatch";
 import { useAppSelector } from "../../../hooks/useAppSelector";
 import { Loader } from "../../shared/Loader";
 import { ConfirmationModal } from "./ConfirmationModal";
+import { UserForm } from "./UserForm";
 
 interface Params {
     id: string;
@@ -29,7 +28,6 @@ export const EditUser: React.FC = () => {
         e.preventDefault();
         const { name, value } = e.target;
         const obj = {[name]: value};
-        console.log(obj);
         dispatch(updateCurrentUser(obj));
     };
 
@@ -41,7 +39,6 @@ export const EditUser: React.FC = () => {
     const handleChecked = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, checked } = e.target;
         const obj = {[name]: checked};
-        console.log(obj);
         dispatch(updateCurrentUser(obj));
     };
 
@@ -52,90 +49,8 @@ export const EditUser: React.FC = () => {
 
     return <>
         {loading 
-            ? <Loader />
-            : <div>
-                <Row>
-                    <Form.Group className="mb-3" as={Col} lg={6}>
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control disabled type="email" name="email" value={user.email} onChange={handleChange} />
-                    </Form.Group>
-                    <Col lg={6} className="d-flex justify-content-evenly align-items-center">
-                        <Form.Check
-                            type="switch"
-                            name="isActive"
-                            checked={user.isActive}
-                            onChange={handleChecked}
-                            label="Active" />
-                        <Form.Check
-                            type="switch"
-                            name="isAdmin"
-                            checked={user.isAdmin}
-                            onChange={handleChecked}
-                            label="Admin" /> 
-                    </Col>
-                </Row>                
-                <Row>
-                    <Form.Group as={Col} lg={6} className="mb-3">
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control type="text" name="first" value={user.first} onChange={handleChange} />
-                    </Form.Group>
-                    <Form.Group as={Col} lg={6} className="mb-3">
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="email" name="last" value={user.last} onChange={handleChange} />
-                    </Form.Group>
-                </Row>
-                <Row>
-                    <Form.Group as={Col} lg={6} className="mb-3">
-                        <Form.Label>Address</Form.Label>
-                        <Form.Control
-                            name="address1"
-                            value={user.address1}
-                            onChange={handleChange} />
-                    </Form.Group>
-                    <Form.Group as={Col} lg={6} className="mb-3">
-                        <Form.Label>Address 2</Form.Label>
-                        <Form.Control
-                            name="address2"
-                            value={user.address2}
-                            onChange={handleChange} />
-                    </Form.Group>
-                </Row>
-                <Row>
-                    <Form.Group as={Col} lg={4} className="mb-3">
-                        <Form.Label>City</Form.Label>
-                        <Form.Control
-                            name="city"
-                            value={user.city}
-                            onChange={handleChange} />
-                    </Form.Group>
-                    <Form.Group as={Col} lg={4} className="mb-3">
-                        <Form.Label>State</Form.Label>
-                        <Form.Select
-                            value={user.state}
-                            name="state"
-                            onChange={handleStateChange}
-                        >
-                            <option>Select...</option>
-                            {stateAbbreviations.map((st, i) => (
-                            <option key={i} value={st}>
-                                {st}
-                            </option>
-                            ))}
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group as={Col} lg={4} className="mb-3">
-                        <Form.Label>Zip</Form.Label>
-                        <Form.Control
-                            name="zip"
-                            value={user.zip}
-                            onChange={handleChange} />
-                    </Form.Group>
-                </Row>
-                <div className="d-flex">
-                    <Button className="ms-auto" onClick={handleShow} disabled={!user.email}>Save</Button>
-                </div>
-            </div>
-        }
+            ? <Loader /> 
+            : <UserForm user={user} onChange={handleChange} onStateChange={handleStateChange} onChecked={handleChecked} onSave={handleShow} />}
         {showModal && <ConfirmationModal onConfirm={handleSubmit} onHide={handleHide} />}
     </>;
 }
---
+++ client/src/components/pages/admin/UserForm.tsx
@@ -0,0 +1,99 @@
+import * as React from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { stateAbbreviations } from "../../../constants/states";
+import { IUser } from "../../../types/IUser";
+
+interface Props {
+    user: IUser;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onStateChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+    onChecked: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onSave: () => void;
+}
+
+export const UserForm: React.FC<Props> = ({user, onChange, onStateChange, onChecked, onSave}) => {
+    return(
+        <>
+            <Row>
+                <Form.Group className="mb-3" as={Col} lg={6}>
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control disabled={!!user.id} type="email" name="email" value={user.email} onChange={onChange} />
+                </Form.Group>
+                <Col lg={6} className="d-flex justify-content-evenly align-items-center">
+                    <Form.Check
+                        type="switch"
+                        name="isActive"
+                        checked={user.isActive}
+                        onChange={onChecked}
+                        label="Active" />
+                    <Form.Check
+                        type="switch"
+                        name="isAdmin"
+                        checked={user.isAdmin}
+                        onChange={onChecked}
+                        label="Admin" /> 
+                </Col>
+            </Row>                
+            <Row>
+                <Form.Group as={Col} lg={6} className="mb-3">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control type="text" name="first" value={user.first} onChange={onChange} />
+                </Form.Group>
+                <Form.Group as={Col} lg={6} className="mb-3">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control type="email" name="last" value={user.last} onChange={onChange} />
+                </Form.Group>
+            </Row>
+            <Row>
+                <Form.Group as={Col} lg={6} className="mb-3">
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control
+                        name="address1"
+                        value={user.address1}
+                        onChange={onChange} />
+                </Form.Group>
+                <Form.Group as={Col} lg={6} className="mb-3">
+                    <Form.Label>Address 2</Form.Label>
+                    <Form.Control
+                        name="address2"
+                        value={user.address2}
+                        onChange={onChange} />
+                </Form.Group>
+            </Row>
+            <Row>
+                <Form.Group as={Col} lg={4} className="mb-3">
+                    <Form.Label>City</Form.Label>
+                    <Form.Control
+                        name="city"
+                        value={user.city}
+                        onChange={onChange} />
+                </Form.Group>
+                <Form.Group as={Col} lg={4} className="mb-3">
+                    <Form.Label>State</Form.Label>
+                    <Form.Select
+                        value={user.state}
+                        name="state"
+                        onChange={onStateChange}
+                    >
+                        <option>Select...</option>
+                        {stateAbbreviations.map((st, i) => (
+                        <option key={i} value={st}>
+                            {st}
+                        </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+                <Form.Group as={Col} lg={4} className="mb-3">
+                    <Form.Label>Zip</Form.Label>
+                    <Form.Control
+                        name="zip"
+                        value={user.zip}
+                        onChange={onChange} />
+                </Form.Group>
+            </Row>
+            <div className="d-flex">
+                <Button className="ms-auto" onClick={onSave} disabled={!user.email}>Save</Button>
+            </div>
+        </>
+    );
+}
