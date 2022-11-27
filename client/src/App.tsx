2a666be3cd66f7e337a29ca3ab5b3197457788b2
Mike Evlantev <mikeev21@gmail.com>
2022-11-27 00:36:41 -0800

    loader for when render is executing (between pages)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,24 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Footer } from "./components/layout/Footer";
 import { Header } from "./components/layout/Header";
 import { Routes } from "./components/routing/Routes";
+import { Loader } from "./components/shared/Loader";
 
 export const App: React.FC = () => {
-    return (
-        <Router>
-            <Header />
-            <main className="py-3">
-                <Route component={Routes} />
-            </main>
-            <Footer />
-        </Router>
-    );
+    const [loading, setLoading] = React.useState(true);
+
+    React.useEffect(() => {
+        setLoading(false);
+    }, []);
+
+    return (<>
+        {loading
+            ? <Loader />
+            : <Router>
+                <Header />
+                <main className="py-3">
+                    <Route component={Routes} />
+                </main>
+                <Footer />
+            </Router>}
+    </>);
 };
