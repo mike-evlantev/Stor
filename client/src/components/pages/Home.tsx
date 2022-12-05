488f5ab0ed193715c46081abaf95d8b567358e32
Mike Evlantev <mikeev21@gmail.com>
2022-12-04 20:42:49 -0800

    more logging

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -8,7 +8,7 @@ import { ProductCard } from "./products/ProductCard";
 
 export const Home: React.FC = () => {
     const dispatch = useAppDispatch();
-    const { loading, products } = useAppSelector(state => state.products);
+    const { loading, products } = useAppSelector(state => {console.log(state); return state.products;});
     console.log(products);
 
     React.useEffect(() => {
